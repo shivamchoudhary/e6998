7da386b351d98878ebe228efb158b94667975fc7
Shivam Choudhary <sc3973@columbia.edu>
2015-09-30 17:53:40 -0400

    Added some boilerplate code to parser

---
+++ service/parser.py
@@ -0,0 +1,17 @@
+import urlparse
+
+class Requests(object):
+    
+    def __init__(self,params):
+        self.param = param
+    def tokenize(self,params):
+        pass
+
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
