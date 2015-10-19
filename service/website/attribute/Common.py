165cd7276d874f6f79f5534871816dab7a351f9e
Shivam Choudhary <sc3973@columbia.edu>
2015-10-19 16:40:36 -0400

    Adding probability

--- service/website/attribute/Common.py
+++ service/website/attribute/Common.py
@@ -2,16 +2,14 @@ import json
 
 
 config = {
-        "age":"0.5",
-        "sex":"0.5",
-        "race":"0.6",
+        "age":0.5,
+        "sex":0.5,
+        "race":0.6,
         "nationality":"0.7"
         }
 
 def read_config():
-    
     base_attributes =[]
-     
     for attr,prob in config.iteritems():
         base_attributes.append(attr)
 
--- service/website/attribute/views.py
+++ service/website/attribute/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from xml.dom.minidom import Document
 import Common
+import random
+
+
 def index(request):
     query_params = request.META['QUERY_STRING']
     dict = tokenize(query_params)
@@ -18,15 +21,27 @@ def tokenize(query_params):
         input_attribute[pairs[0]] = pairs[1]
     return input_attribute
 
-
-
 def make_dom(dict):
     doc = Document()
     base = doc.createElement("Attributes")
     x = doc.appendChild(base)
+    base_attributes = Common.read_config()
     for k,v in dict.iteritems():
-        element = doc.createElement(k)
-        content = doc.createTextNode(v)
-        element.appendChild(content)
-        base.appendChild(element)
+        if k in base_attributes:
+            p = random.random()
+            print Common.config[k]
+            if p <=Common.config[k]:
+                element = doc.createElement(k)
+                content = doc.createTextNode(v)
+                element.appendChild(content)
+                base.appendChild(element)
+            elif p>Common.config[k]:
+                print '1'
+                element = doc.createElement(k)
+                content = doc.createTextNode("Fixed")
+                element.appendChild(content)
+                base.appendChild(element)
+                
+                
+    print base.toprettyxml()
     return base.toprettyxml()
