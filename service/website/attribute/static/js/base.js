76c6d0e8cb7a205170bd5ad77f7ed9776225119b
ClassWYZ <wenyuzhang@outlook.com>
2015-11-13 15:02:16 -0500

    chart updated

--- service/website/attribute/static/js/base.js
+++ service/website/attribute/static/js/base.js
@@ -23,3 +23,29 @@ function readInput() {
     return dict;
 }
 
+$(function() {
+
+var data = [
+      { y: '0.6', a: 90,  b: 70},
+      { y: '0.7', a: 50, b: 90},
+      { y: '0.8', a: 65,  b: 75},
+      { y: '0.9', a: 50,  b: 50},
+      { y: '1.0', a: 160, b: 95}
+    ],
+    config = {
+      data: data,
+      xkey: 'y',
+      ykeys: ['a', 'b'],
+      labels: ['Detected P', 'Generated P'],
+      fillOpacity: 0.6,
+      hideHover: 'auto',
+      behaveLikeLine: true,
+      resize: true,
+      parseTime: false,
+      pointFillColors:['#ffffff'],
+      pointStrokeColors: ['black'],
+      lineColors:['gray','red']
+  };
+config.element = 'line-chart';
+Morris.Line(config);
+});
