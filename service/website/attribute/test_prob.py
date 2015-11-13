b68bc33be7f1c26f3fa408a01e95da4cd095cf8f
Shivam Choudhary <sc3973@columbia.edu>
2015-11-13 16:09:57 -0500

    Adding automated probability test

--- service/website/attribute/test_prob.py
+++ service/website/attribute/test_prob.py
@@ -1,11 +1,31 @@
 import views
+import Common
+import unittest
+iterations = 1000
 def testing():
-    india_counter = 0
-    global_counter = 0
-    attr = {'country':'indian'}
-    for i in range(0,10000):
-        if views.dry_run(attr)=="indian":
-            india_counter+=1
-        global_counter+=1
-    print float(india_counter)/global_counter
-testing() 
+    config = Common.config
+    for k,v in config.iteritems():
+        for k1, val1 in v.iteritems():
+            output = {}
+            for i in range(0,iterations):
+                result = views.make_dom({k:k1})
+                result = result[k]
+                try:
+                    output[result] +=1
+                except KeyError:
+                    output[result] = 1
+            print output
+            for prob,values in val1.iteritems():
+                try:
+                    est_prob = output[values]/float (iterations)
+                except KeyError:
+                    continue
+                if est_prob-prob <=0.1:
+                    print "Success",k,k1,values
+                else:
+                    print "Try with more iterations"
+
+
+
+testing()
+
