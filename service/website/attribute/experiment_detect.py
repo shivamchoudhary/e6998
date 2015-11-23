6d9be17aca1f62daf15d35ba6418ed6bbc1f90bb
Shivam Choudhary <sc3973@columbia.edu>
2015-11-23 18:30:44 -0500

    Adding code to detect pvals

---
+++ service/website/attribute/experiment_detect.py
@@ -0,0 +1,28 @@
+import json
+import os
+import Common
+
+class pvals(object):
+    def __init__(self):
+        self.pvalf = Common.BASE_DIR+"/pvalues.json"
+        self.success = 0
+        self.threshold_detection  = 10^-3
+        self.load_pvals(self.pvalf) 
+    def num_experiment(self):
+        return len(self.pvals.keys())
+    def num_success(self):
+        for k,v in self.pvals.iteritems():
+            if v <self.threshold_detection:
+                self.success+=1
+        return self.success
+
+    def load_pvals(self,fname):
+        with open(self.pvalf) as pvalf:
+            self.pvals = json.load(pvalf)
+        return self.pvals
+
+
+
+p = pvals()
+print p.num_success()
+
