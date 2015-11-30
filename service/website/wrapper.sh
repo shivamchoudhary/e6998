2f2434672939ecb1ba9ef52cb273d9dca9a54687
Shivam Choudhary <sc3973@columbia.edu>
2015-11-30 15:38:19 -0500

    Adding wrapper changes this is crazy we need to find a better way else Angels would die

--- service/website/wrapper.sh
+++ service/website/wrapper.sh
@@ -4,5 +4,5 @@ python testbench_wrapper.py $NUM_BROWSERS $ITERATIONS $sites $fname
 cd /home/shivam/e6998/service/website/attribute
 python dict_creator.py $fname
 cd $dataObservatory_dir
-pwd
 python3 test_dataobservatory.py $fname
+mv "$fname.json" /home/shivam/e6998/service/website/attribute/pvalues
