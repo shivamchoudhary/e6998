c51f127ba29f834d0db39b0ff408c5df5bc7f432
Shivam Choudhary <sc3973@columbia.edu>
2015-11-30 16:13:08 -0500

    Adding changes for pvals

--- service/website/attribute/experiment_detect.py
+++ service/website/attribute/experiment_detect.py
@@ -1,35 +1,39 @@
 import json
 import os
 import Common
-filename = ['pvalues0.1.json',
-        'pvalues0.5.json',
-        'pvalues0.9.json']
 class pvals(object):
-    def __init__(self):
-        self.prob = {0.1:"",
-                0.5:"",
-                0.9:""}
+    def __init__(self,filelist):
+        self.filelist = filelist
+        self.prob = {}
         self.threshold_detection  = 10^-3
+        self.run_exp()
     def run_exp(self):
-        for files in filename:
-            self.pvals = self.load_pvals(files)
-            probability = files.split(".json")[0]
-            probability = float(probability.split("pvalues")[1])
-            self.prob[probability] = self.num_success()
+        for files in self.filelist:
+            pvals = self.load_pvals(files)
+            probability = str(files.split(".json")[0].split("prob_")[1])
+            self.prob[probability] = self.num_success(pvals)
         return self.prob
-    def num_experiment(self):
-        return len(self.pvals.keys())
-    def num_success(self):
-        self.success = 0
-        for k,v in self.pvals.iteritems():
+    def num_experiment(self,pvals):
+        return len(pvals.keys())
+    def num_success(self,pvals):
+        success = 0
+        for k,v in pvals.iteritems():
             if v <self.threshold_detection:
-                self.success+=1
-        return self.success
+                success+=1
+        return success
     def load_pvals(self,fname):
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        BASE_DIR = os.path.join(BASE_DIR,"attribute/") 
+        BASE_DIR = os.path.join(BASE_DIR,"attribute/pvalues/") 
         print BASE_DIR + fname
         with open(BASE_DIR + fname) as pvalf:
-            self.pvals = json.load(pvalf)
-        return self.pvals
+            pvals = json.load(pvalf)
+        return pvals
 
+def main():
+    pvaluefiles = []
+    probability = {}
+    pvaluefiles += [each for each in os.listdir(os.curdir+"/pvalues") 
+            if each.endswith('.json')]
+    p = pvals(pvaluefiles)
+if __name__ =="__main__":
+    main()
