fb42a4aab17c0a8b11f1c73f5cea4b95fbc8a57c
Shivam Choudhary <sc3973@columbia.edu>
2015-12-10 15:48:07 -0500

    Adding multilevel dict

--- service/website/attribute/dict_creator.py
+++ service/website/attribute/dict_creator.py
@@ -7,18 +7,18 @@ def create_dict():
     for file in os.listdir("/home/shivam/OpenWPM/demo_results"):
         if file.endswith(".html"):
             attr_dict[file]={}
-            name = file.split(".")[0]
-            geo = name.split("=")[1]
-            attr_dict[file]["geo"]=geo
+            names = file.split("&")
+            for name in names:
+                geo = name.split("=")[1]
+                if not geo.endswith(".html"):
+                    attr_dict[file]["geo"]=geo
     attr_dict["all_values"] = {}
     all_geo = ["germany","india"]
     attr_dict["all_values"]["geo"]=all_geo
     with open("/home/shivam/OpenWPM/demo_results/localhost.json",'w') as outfile:
         json.dump(attr_dict,outfile,indent=4)
 
-
 def main():
     create_dict()
-
 if __name__=="__main__":
     main()
